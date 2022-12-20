got cif training running, implemented frame & atom masking for PAE and PDE losses (fixed transpose "bug" in pae)

data_loader.py
@@ -71,9 +71,9 @@ def set_data_loader_params(args):
         "VAL_RNA"          : "%s/rna_valid.csv"%na_dir,
         "VAL_COMPL"        : "%s/val_lists/xaa"%compl_dir,
         "VAL_NEG"          : "%s/val_lists/xaa.neg"%compl_dir,
-        "VAL_SM_STRICT"    : "%s/list_v02_smcompl_validstrict_20221102.csv"%sm_compl_dir, 
+        "VAL_SM_STRICT"    : "%s/sm_compl_valid_strict_20221216.csv"%sm_compl_dir, 
         "TEST_SM"          : "%s/sm_test_heldout_test_clusters.txt"%sm_compl_dir,
-        "DATAPKL"          : "%s/dataset_20221219.pkl"%sm_compl_dir, # cache for faster loading 
+        "DATAPKL"          : "%s/dataset_20221220.pkl"%sm_compl_dir, # cache for faster loading 
         "PDB_DIR"          : base_dir,
         "FB_DIR"           : fb_dir,
         "COMPL_DIR"        : compl_dir,
@@ -157,7 +157,8 @@ def MSAFeaturize(msa, ins, params, p_mask=0.15, eps=1e-6, nmer=1, L_s=[], tocpu=
             term_info[start, 0] = 1.0 # flag for N-term
             term_info[start+L_chain-1,1] = 1.0 # flag for C-term
             start += L_chain
-    binding_site = torch.zeros((L,1), device=msa.device).float()
+    #binding_site = torch.zeros((L,1), device=msa.device).float()
+    binding_site = torch.zeros((L,0), device=msa.device).float() # keeping this off for now (Jue 12/19)
     # raw MSA profile
     raw_profile = torch.nn.functional.one_hot(msa, num_classes=NAATOKENS)
     raw_profile = raw_profile.float().mean(dim=0) 
@@ -576,25 +577,7 @@ def get_train_valid_set(params, OFFSET=1000000):
         train_sm_compl_covale, valid_sm_compl_covale, sm_compl_covale_IDs, sm_compl_covale_weights = \
             _parse_sm_compl_df(params['SM_COVALE_LIST'], eval_cols=['COVALENT', 'LIGAND', 'LIGXF', 'PARTNERS'])
 
-        # stricter versions of protein-small mol validation set
-        def _make_example_dict(df, make_entry_func, cluster_key='CLUSTER'):
-            """Converts a dataframe of training examples to dictionary where keys are cluster IDs and values
-               are lists of training examples formatted according to the function `make_entry_func`."""
-            data_dict = {}
-            for i,row in df.iterrows():
-                entry = make_entry_func(row)
-                cluster = row[cluster_key] # cluster of this example
-                if cluster in data_dict.keys():
-                    data_dict[cluster].append(entry)
-                else:
-                    data_dict[cluster] = [entry]
-            return data_dict
-        def _make_entry(row):
-            return ((row.CHAINID, row.HASH, row.LIGANDS), row.LEN_EXIST, row.WEIGHT)
-
-        df = load_df(params['VAL_SM_STRICT'], params, eval_cols=['LIGANDS']) # removed ligand cluster overlap with train
-        df['WEIGHT'] = 1 # examples have already been redundancy-reduced
-        valid_sm_compl_strict = _make_example_dict(df, make_entry_func=_make_entry, cluster_key='ID') # use all examples regardless of sequence cluster
+        valid_sm_compl_strict = load_df(params['VAL_SM_STRICT'], params, eval_cols=['LIGAND','LIGXF','PARTNERS'])
 
         # cambridge small molecule crystals
         sim_idx = int(params["MAXSIM"]*100-50)
@@ -2139,6 +2122,21 @@ def crop_sm_compl(prot_xyz, lig_xyz,Ls, params):
     lig_sel = torch.arange(lig_xyz.shape[0])+Ls[0]
     return torch.cat((sel, lig_sel))
 
+def unbatch_item(item):
+    """Flattens batched dictionaries returned from dataloaders to remove unecessary nested lists"""
+    def flatten_value(v):
+        if (type(v) is list and len(v)==1) or \
+           (type(v) is torch.Tensor and len(v.shape)>0 and v.shape[0]==1):
+            v = v[0]
+        if (type(v) is list and len(v)>1):
+            for i,x in enumerate(v):
+                v[i] = flatten_value(x)
+        return v
+
+    new_item = dict()
+    for k in item:
+        new_item[k] = flatten_value(item[k])
+    return new_item
 
 class Dataset(data.Dataset):
     def __init__(self, IDs, loader, item_dict, params, homo, unclamp_cut=0.9, pick_top=True, p_homo_cut=-1.0, n_res_atomize=0, flank=0, seed=None):
@@ -2507,7 +2505,7 @@ class DistributedWeightedSampler(data.Sampler):
         assert num_example_per_epoch % num_replicas == 0
         total_examples = fraction_pdb + fraction_fb + fraction_compl + fraction_na_compl + \
                 fraction_rna + fraction_sm_compl + fraction_metal_compl + fraction_sm_compl_multi + \
-                fraction_sm + fraction_atomize_pdb
+                fraction_sm_compl_covale + fraction_sm + fraction_atomize_pdb
         assert (np.allclose(total_examples, 1.0)), f"Fractions of datasets add up to {total_examples}, should add up to 1.0"
 
         self.dataset = dataset
@@ -2636,9 +2634,10 @@ class DistributedWeightedSampler(data.Sampler):
         offset += len(self.dataset.ID_dict['sm_compl_multi'])
         
         if (self.num_sm_compl_covale_per_epoch>0):
-            sm_compl_covale_sampled = torch.multinomial(self.weights_dict['sm_compl_multi'], self.num_sm_compl_multi_per_epoch, self.replacement, generator=g)
+            sm_compl_covale_sampled = torch.multinomial(self.weights_dict['sm_compl_covale'], self.num_sm_compl_covale_per_epoch, self.replacement, generator=g)
             sel_indices = torch.cat((sel_indices, indices[sm_compl_covale_sampled + offset]))
         offset += len(self.dataset.ID_dict['sm_compl_covale'])
+
         if (self.num_sm_per_epoch>0):
             sm_sampled = torch.multinomial(self.weights_dict['sm'], self.num_sm_per_epoch, self.replacement, generator=g)
             sel_indices = torch.cat((sel_indices, indices[sm_sampled + offset]))
@@ -2654,6 +2653,8 @@ class DistributedWeightedSampler(data.Sampler):
 
         # per each gpu
         indices = indices[self.rank:self.total_size:self.num_replicas]
+        print('len(indices)',len(indices))
+        print('self.num_samples',self.num_samples)
         assert len(indices) == self.num_samples
 
         return iter(indices.tolist())

loss.py
@@ -130,28 +130,47 @@ def compute_FAPE(Rs, Ts, xs, Rsnat, Tsnat, xsnat, Z=10.0, dclamp=10.0, eps=1e-4)
 
     return loss
 
-def compute_pae_loss(X, X_y, uX, Y, Y_y, uY, logit_pae, pae_bin_step=0.5, eps=1e-4):
-    # predicted aligned error: C-alpha (or sm. mol atom) distances in backbone frames
-    xij_ca = torch.einsum('rji,rsj->rsi', uX[-1,:,0], X[-1,:,None,1] - X_y[-1,None,:,0,:]) # last bb prediction
-    xij_ca_t = torch.einsum('rji,rsj->rsi', uY[0,:,0], Y[0,:,None,1] - Y_y[0,None,:,0,:]) # assumes B=1
+def compute_pae_loss(X, X_y, uX, Y, Y_y, uY, logit_pae, frame_mask, atom_mask, pae_bin_step=0.5, eps=1e-4):
+    """Predicted Aligned Error: C-alpha (or sm. mol atom) distances in backbone frames from final layer"""
+
+    frame_mask_bb = frame_mask[0,:,0] # valid backbone frames (L,)
+    atom_mask_ca = atom_mask[0,:,1] # valid CA atoms (L,)
+
+    xij_ca = torch.einsum(
+        'fji,faj->fai',
+        uX[-1,frame_mask_bb,0],
+        X[-1,None,atom_mask_ca,1] - X_y[-1,frame_mask_bb,None,0]
+    ) # (N_valid_frames, N_valid_ca, 3)
+
+    xij_ca_t = torch.einsum(
+        'fji,faj->fai',
+        uY[-1,frame_mask_bb,0],
+        Y[-1,None,atom_mask_ca,1] - Y_y[-1,frame_mask_bb,None,0]
+    ) # (N_valid_frames, N_valid_ca, 3)
+
     eij_label = torch.sqrt(torch.square(xij_ca - xij_ca_t).sum(dim=-1)+eps).clone().detach()
 
     nbin = logit_pae.shape[1]
     pae_bins = torch.linspace(pae_bin_step, pae_bin_step*(nbin-1), nbin-1, dtype=logit_pae.dtype, device=logit_pae.device)
     true_pae_label = torch.bucketize(eij_label, pae_bins, right=True).long()
-    return torch.nn.CrossEntropyLoss(reduction='mean')(logit_pae, true_pae_label[None]) # assumes B=1
 
-def compute_pde_loss(X, Y, logit_pde, pde_bin_step=0.3):
-    # predicted distance error: C-alpha (or sm. mol atom) pairwise distances
-    dX = torch.cdist(X[-1,:,1], X[-1,:,1], compute_mode='donot_use_mm_for_euclid_dist')
-    dY = torch.cdist(Y[0,:,1], Y[0,:,1], compute_mode='donot_use_mm_for_euclid_dist')
+    logit_pae_masked = logit_pae[:,:,frame_mask_bb][...,atom_mask_ca] # (1, nbins, N_valid_frames, N_valid_ca)
+
+    return torch.nn.CrossEntropyLoss(reduction='mean')(logit_pae_masked, true_pae_label[None]) # assumes B=1
+
+def compute_pde_loss(X, Y, logit_pde, atom_mask, pde_bin_step=0.3):
+    """Predicted Distance Error: C-alpha (or sm. mol atom) pairwise distances"""
+    atom_mask_ca = atom_mask[0,:,1] # valid CA atoms (L,)
+
+    dX = torch.cdist(X[-1,atom_mask_ca,1], X[-1,atom_mask_ca,1], compute_mode='donot_use_mm_for_euclid_dist')
+    dY = torch.cdist(Y[0,atom_mask_ca,1], Y[0,atom_mask_ca,1], compute_mode='donot_use_mm_for_euclid_dist')
     dist_err = torch.abs(dX-dY).clone().detach()
 
     nbin = logit_pde.shape[1]
     pde_bins = torch.linspace(pde_bin_step, pde_bin_step*(nbin-1), nbin-1, dtype=logit_pde.dtype, device=logit_pde.device)
     true_pde_label = torch.bucketize(dist_err, pde_bins, right=True).long()
-    return torch.nn.CrossEntropyLoss(reduction='mean')(logit_pde, true_pde_label[None]) # assumes B=1
-
+    logit_pde_masked = logit_pde[:,:,atom_mask_ca][...,atom_mask_ca] # (1, nbins, N_valid_ca, N_valid_ca)
+    return torch.nn.CrossEntropyLoss(reduction='mean')(logit_pde_masked, true_pde_label[None]) # assumes B=1
 
 # from Ivan: FAPE generalized over atom sets & frames
 def compute_general_FAPE(X, Y, atom_mask, frames, frame_mask, frame_atom_mask=None, 
@@ -201,9 +220,10 @@ def compute_general_FAPE(X, Y, atom_mask, frames, frame_mask, frame_atom_mask=No
     diff = torch.sqrt( torch.sum( torch.square(xij-xij_t[None,...]), dim=-1 ) + eps )
 
     loss = (1.0/Z) * (torch.clamp(diff, max=dclamp)).mean(dim=(1,2))
-    pae_loss = compute_pae_loss(X, X_y, uX, Y, Y_y, uY, logit_pae) if logit_pae is not None \
+    pae_loss = compute_pae_loss(X, X_y, uX, Y, Y_y, uY, logit_pae, frame_mask, atom_mask) \
+                   if logit_pae is not None \
                    else torch.tensor(0).to(frames.device)
-    pde_loss = compute_pde_loss(X, Y, logit_pde) if logit_pde is not None \
+    pde_loss = compute_pde_loss(X, Y, logit_pde, atom_mask) if logit_pde is not None \
                    else torch.tensor(0).to(frames.device)
 
     return loss, pae_loss, pde_loss

train_multi_EMA.py
@@ -9,8 +9,11 @@ import torch.nn as nn
 from torch.utils import data
 from functools import partial
 from data_loader import (
-    get_train_valid_set, loader_pdb, loader_fb, loader_complex, loader_na_complex, loader_rna, loader_sm, loader_sm_compl, loader_sm_compl_covale, loader_atomize_pdb, 
-    Dataset, DatasetComplex, DatasetNAComplex, DatasetRNA, DatasetSM, DatasetSMComplex, DistilledDataset, DistributedWeightedSampler
+    get_train_valid_set, loader_pdb, loader_fb, loader_complex,
+    loader_na_complex, loader_rna, loader_sm, loader_sm_compl, loader_sm_compl_covale,
+    loader_atomize_pdb, Dataset, DatasetComplex, DatasetNAComplex, DatasetRNA,
+    DatasetSM, DatasetSMComplex, DistilledDataset, DistributedWeightedSampler,
+    unbatch_item
 )
 from kinematics import xyz_to_c6d, c6d_to_bins, xyz_to_t2d, xyz_to_bbtor
 from RoseTTAFoldModel  import RoseTTAFoldModule
@@ -184,11 +187,26 @@ class Trainer():
         assert (B==1) # fd - code assumes a batch size of 1
         
         tot_loss = 0.0
-        
+
+        # set up frames
+        frames, frame_mask = get_frames(
+            pred_allatom[-1,None,...], mask_crds, seq, self.fi_dev, atom_frames)
+        # update frames and frames_mask to only include BB frames (have to update both for compatibility with compute_general_FAPE)
+        frames_BB = frames.clone()
+        frames_BB[..., 1:, :, :] = 0
+        frame_mask_BB = frame_mask.clone()
+        frame_mask_BB[...,1:] =False
+       
         # c6d loss
         for i in range(4):
             loss = self.loss_fn(logit_s[i], label_s[...,i]) # (B, L, L)
-            loss = (mask_2d*loss).sum() / (mask_2d.sum() + eps)
+            if i==0: # apply distogram loss to all residue pairs with valid BB atoms
+                mask_2d_ = mask_2d
+            else: # apply anglegram loss only when both residues have valid BB frames (i.e. not metal ions)
+                bb_frame_good = frame_mask[:,:,0]
+                loss_mask_2d = bb_frame_good & bb_frame_good[...,None]
+                mask_2d_ = mask_2d & loss_mask_2d
+            loss = (mask_2d_*loss).sum() / (mask_2d_.sum() + eps)
             tot_loss += w_dist*loss
             loss_dict[f'c6d_{i}'] = loss.detach()
 
@@ -203,14 +221,6 @@ class Trainer():
         # Structural loss (layer-wise backbone FAPE)
         dclamp = 300.0 if unclamp else 30.0 # protein & NA FAPE distance cutoffs
         dclamp_sm, Z_sm = 4, 4  # sm mol FAPE distance cutoffs
-
-        frames, frame_mask = get_frames(
-            pred_allatom[-1,None,...], mask_crds, seq, self.fi_dev, atom_frames)
-        # update frames and frames_mask to only include BB frames (have to update both for compatibility with compute_general_FAPE)
-        frames_BB = frames.clone()
-        frames_BB[..., 1:, :, :] = 0
-        frame_mask_BB = frame_mask.clone()
-        frame_mask_BB[...,1:] =False
 
         L1 = same_chain[0,0,:].sum()
         mask_BBA = mask_BB.clone()
@@ -280,6 +290,7 @@ class Trainer():
         loss_dict['pae_loss'] = pae_loss.detach()
         loss_dict['pde_loss'] = pde_loss.detach()
 
+        # small-molecule ligands
         sm_res_mask = is_atom(label_aa_s[0,0])*mask_BB[0] # (L,)
         if bool(torch.any(sm_res_mask)):
             # ligand fape (layer-averaged fape on atom coordinates with atom frames)
@@ -769,6 +780,7 @@ class Trainer():
         valid_ID_dict['atomize_pdb'] = train_ID_dict['pdb']
         weights_dict['atomize_pdb'] = weights_dict['pdb']
         train_dict['atomize_pdb'] = train_dict['pdb']
+        valid_ID_dict['atomize_pdb'] = valid_ID_dict['pdb']
         valid_dict['atomize_pdb'] = valid_dict['pdb']
 
         if self.dataset_param['n_valid_pdb'] is None: 
@@ -902,11 +914,6 @@ class Trainer():
                 loader_sm_compl, valid_dict['sm_compl'],
                 self.loader_param,
             ),
-            sm_compl_strict = DatasetSMComplex(
-                valid_ID_dict['sm_compl_strict'][:self.dataset_param['n_valid_sm_compl_strict']],
-                loader_sm_compl, valid_dict['sm_compl_strict'],
-                self.loader_param, task='sm_compl_strict'
-            ),
             metal_compl = DatasetSMComplex(
                 valid_ID_dict['metal_compl'][:self.dataset_param['n_valid_metal_compl']],
                 loader_sm_compl, valid_dict['metal_compl'],
@@ -916,6 +923,11 @@ class Trainer():
                 valid_ID_dict['sm_compl_multi'][:self.dataset_param['n_valid_sm_compl_multi']],
                 loader_sm_compl, valid_dict['sm_compl_multi'],
                 self.loader_param, task='sm_compl_multi'
+            ),
+            sm_compl_strict = DatasetSMComplex(
+                valid_ID_dict['sm_compl_strict'][:self.dataset_param['n_valid_sm_compl_strict']],
+                loader_sm_compl, valid_dict['sm_compl_strict'],
+                self.loader_param, task='sm_compl_strict'
             ),
             sm_compl_covale = DatasetSMComplex(
                 valid_ID_dict['sm_compl_covale'][:self.dataset_param['n_valid_sm_compl_covale']],
@@ -1066,7 +1078,7 @@ class Trainer():
                     v, rank, gpu, world_size, epoch, rng, header=valid_headers[k], 
                     verbose = self.eval) 
 
-                if k == 'SM Compl':
+                if k == 'sm_compl':
                     valid_tot, valid_loss, valid_acc = valid_tot_, valid_loss_, valid_acc_
 
             if self.eval: break
@@ -1256,7 +1268,8 @@ class Trainer():
                         )
 
                         true_crds_, atom_mask_ = resolve_equiv_natives(pred_crds[-1], true_crds, atom_mask)
-                        res_mask = ~((atom_mask_[:,:,:3].sum(dim=-1) < 3.0) * ~(is_atom(msa[:,i_cycle,0])))
+                        #res_mask = ~((atom_mask_[:,:,:3].sum(dim=-1) < 3.0) * ~(is_atom(msa[:,i_cycle,0])))
+                        res_mask = get_prot_sm_mask(atom_mask_, msa[0,i_cycle,0])
                         mask_2d = res_mask[:,None,:] * res_mask[:,:,None]
 
                         true_crds_frame = xyz_to_frame_xyz(true_crds_, msa[:, i_cycle, 0],atom_frames)
@@ -1305,8 +1318,8 @@ class Trainer():
                     )
 
                     true_crds_, atom_mask_ = resolve_equiv_natives(pred_crds[-1], true_crds, atom_mask)
-                    res_mask = ~((atom_mask_[0,:,:3][None].sum(dim=-1) < 3.0) * ~(is_atom(msa[:, i_cycle,0])))
-
+                    #res_mask = ~((atom_mask_[:,:,:3].sum(dim=-1) < 3.0) * ~(is_atom(msa[:,i_cycle,0])))
+                    res_mask = get_prot_sm_mask(atom_mask_, msa[0,i_cycle,0])
                     mask_2d = res_mask[:,None,:] * res_mask[:,:,None]
 
                     true_crds_frame = xyz_to_frame_xyz(true_crds_, msa[:, i_cycle, 0],atom_frames)
@@ -1341,12 +1354,18 @@ class Trainer():
                     scheduler.step()
                 ddp_model.module.update() # apply EMA
 
+            item_ = unbatch_item(item) # remove nested lists to make more readable when printed
+
             if torch.isnan(loss):
-                print('nan loss',item)
+                print('nan loss',item_)
                 save_pdbs = True
 
             if task[0].startswith('sm_compl') or task[0].startswith('metal_compl'):
-                name = f"{item['CHAINID'][0]}_{item['LIGAND'][0][2][0]}"
+                if type(item_['LIGAND'][0]) is list: # multires or covalent ligands
+                    lig_str = '_'.join([x[0]+x[1]+'-'+x[2] for x in item_['LIGAND']])[:20]
+                else:
+                    lig_str = item_['LIGAND'][0]+item_['LIGAND'][1]+'-'+item_['LIGAND'][2]
+                name = item_['CHAINID']+'_asm'+str(int(item_['ASSEMBLY']))+'_'+lig_str
             elif task[0]=='sm_only':
                 name = item[0]
             else:
@@ -1572,7 +1591,8 @@ class Trainer():
 
                 true_crds_, atom_mask_ = resolve_equiv_natives(pred_crds[-1], true_crds, atom_mask)
 
-                res_mask = ~((atom_mask_[:,:,:3].sum(dim=-1) < 3.0) * ~(is_atom(msa[:,i_cycle,0])))
+                #res_mask = ~((atom_mask_[:,:,:3].sum(dim=-1) < 3.0) * ~(is_atom(msa[:,i_cycle,0])))
+                res_mask = get_prot_sm_mask(atom_mask_, msa[0,i_cycle,0])
                 mask_2d = res_mask[:,None,:] * res_mask[:,:,None]
 
                 true_crds_frame = xyz_to_frame_xyz(true_crds_, msa[:, i_cycle, 0],atom_frames)
@@ -1600,8 +1620,13 @@ class Trainer():
                 valid_acc += acc_s.detach()
 
                 # records results
-                if task[0].startswith('sm_compl'):
-                    name = item[2][0][0].replace('.mol2','')
+                if task[0].startswith('sm_compl') or task[0].startswith('metal_compl'):
+                    item_ = unbatch_item(item)
+                    if type(item_['LIGAND'][0]) is list: # multires or covalent ligands
+                        lig_str = '_'.join([x[0]+x[1]+'-'+x[2] for x in item_['LIGAND']])[:20]
+                    else:
+                        lig_str = item_['LIGAND'][0]+item_['LIGAND'][1]+'-'+item_['LIGAND'][2]
+                    name = item_['CHAINID']+'_asm'+str(int(item_['ASSEMBLY']))+'_'+lig_str
                 elif task[0]=='sm_only':
                     name = item[0]
                 else: