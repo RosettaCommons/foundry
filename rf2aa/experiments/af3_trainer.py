bugfix for confidence head to not just compute average

rf2aa/experiments/af3_trainer.py
@@ -355,6 +355,7 @@ class AF3TrainerRollout(AF3Trainer):
             # loss_input["X_gt_I_symm"].to(gpu),
             # loss_input["crd_mask_I_symm"].to(gpu),
             # loss_input["seq"].to(gpu),
+            frame_atom_idxs=loss_input['frame_atom_idxs'],
             no_sync=self.model.no_sync,
         )
 
@@ -424,44 +425,88 @@ class AF3TrainerRollout(AF3Trainer):
         new_shadow_state = {}
         state_dict = self.model.module.model.state_dict()
 
+        # def merge_torch_weights(first_file_path, second_file_path, output_file_path):
+        #     """
+        #     Merge PyTorch weight files with parameter renaming and filtering.
+            
+        #     Args:
+        #     first_file_path (str): Path to the first .pt weight file
+        #     second_file_path (str): Path to the second .pt weight file
+        #     output_file_path (str): Path to save the merged weight file
+        #     """
+        #     # Load the first checkpoint
+        #     first_checkpoint = torch.load(first_file_path)
+            
+        #     # Load the second checkpoint
+        #     second_checkpoint = torch.load(second_file_path)
+            
+        #     # Create a new state dict
+        #     merged_state_dict = {}
+        #     merged_final_state_dict = {}
+            
+        #     # Rename parameters from the first checkpoint and add 'model.' prefix
+        #     for key, value in first_checkpoint['model_state_dict'].items():
+        #         print(f'Renaming {key} to model.{key}')
+        #         merged_state_dict[f'model.{key}'] = value
+        #     for key, value in first_checkpoint['final_state_dict'].items():
+        #         print(f'Renaming {key} to model.{key}')
+        #         merged_final_state_dict[f'model.{key}'] = value
+            
+        #     # Add parameters from the second checkpoint that contain 'confidence'
+        #     for key, value in second_checkpoint['model_state_dict'].items():
+        #         if 'confidence' in key:
+        #             print(f'Adding {key}')
+        #             merged_state_dict[key] = value
+        #     for key, value in second_checkpoint['final_state_dict'].items():
+        #         if 'confidence' in key:
+        #             print(f'Adding {key}')
+        #             merged_final_state_dict[key] = value
 
-        if self.config.training_params.reset_optimizer_params:
-            #get around a loading issue - I'll only need to do this when loading from a weight set trained on something other than the rollout
-            # Create a new dictionary to store the modified keys
-            new_model_state_dict = {}
+            
+        #     # overwrite state dicts with merged ones
+        #     first_checkpoint['model_state_dict'] = merged_state_dict
+        #     first_checkpoint['final_state_dict'] = merged_final_state_dict
 
-            # Iterate over the original dictionary
-            for param in self.checkpoint['model_state_dict']:
-                # Modify the key
-                new_param = 'model.' + param
-                # Add the modified key and its value to the new dictionary
-                new_model_state_dict[new_param] = self.checkpoint['model_state_dict'][param]
+        #     #overwrite the optimizer with the second checkpoint's optimizer
+        #     # first_checkpoint['optimizer_state_dict'] = second_checkpoint['optimizer_state_dict']
+        #     # first_checkpoint['scheduler_state_dict'] = second_checkpoint['scheduler_state_dict']
+        #     # first_checkpoint['scaler_state_dict'] = second_checkpoint['scaler_state_dict']
+            
+        #     # Save the merged checkpoint
+        #     torch.save(first_checkpoint, output_file_path)
+            
+        #     print(f"Merged weights saved to {output_file_path}")
 
-            # #get the confidence params
-            # for param in self.conf_checkpoint['model_state_dict']:
-            #     if 'confidence' in param:
-            #         new_model_state_dict[param] = self.conf_checkpoint['model_state_dict'][param]
+        # first = '/home/tuscant/weights/rf2aa-af3-repro2_240.pt'
+        # second = '/home/tuscant/code/af3_pae/RF2-allatom/rf2aa/output/rf2aa-af3-repro-rollout_last.pt'
+        # product = '/home/tuscant/weights/240_merged_confidence_5.pt'
+        # merge_torch_weights(first, second, product)
+        # print('merged weights')
+        # print(donemerging)
 
-            # Replace the original dictionary with the new one
-            self.checkpoint['model_state_dict'] = new_model_state_dict
+        # if self.config.training_params.reset_optimizer_params:
+        #     #get around a loading issue - I'll only need to do this when loading from a weight set trained on something other than the rollout
+        #     # Create a new dictionary to store the modified keys
+        #     new_model_state_dict = {}
 
+        #     # Iterate over the original dictionary
+        #     for param in self.checkpoint['model_state_dict']:
+        #         # Modify the key
+        #         new_param = 'model.' + param
+        #         # Add the modified key and its value to the new dictionary
+        #         new_model_state_dict[new_param] = self.checkpoint['model_state_dict'][param]
 
-            #Do the same for the final_state_dict
-            new_model_state_dict = {}
-            for param in self.checkpoint['final_state_dict']:
-                # Modify the key
-                new_param = 'model.' + param
-                # Add the modified key and its value to the new dictionary
-                new_model_state_dict[new_param] = self.checkpoint['final_state_dict'][param]
-            self.checkpoint['final_state_dict'] = new_model_state_dict
+        #     # Replace the original dictionary with the new one
+        #     self.checkpoint['model_state_dict'] = new_model_state_dict
 
-            # #get the confidence params
-            # for param in self.conf_checkpoint['final_state_dict']:
-            #     if 'confidence' in param:
-            #         new_model_state_dict[param] = self.conf_checkpoint['final_state_dict'][param]
-
-
-
+        #     #Do the same for the final_state_dict
+        #     new_model_state_dict = {}
+        #     for param in self.checkpoint['final_state_dict']:
+        #         # Modify the key
+        #         new_param = 'model.' + param
+        #         # Add the modified key and its value to the new dictionary
+        #         new_model_state_dict[new_param] = self.checkpoint['final_state_dict'][param]
+        #     self.checkpoint['final_state_dict'] = new_model_state_dict
 
         for param in state_dict:
             if param not in self.checkpoint['model_state_dict']:
@@ -540,7 +585,7 @@ class AF3TrainerRollout(AF3Trainer):
                     self.load_scheduler()
                     self.load_scaler()
                 else:
-                    warnings.warn(f"User specified reset_optimizer_params=False. Did not load optimizer values from checkpoint")
+                    warnings.warn(f"User specified reset_optimizer_params=True. Did not load optimizer values from checkpoint")
             self.checkpoint = None # unload checkpoint dict
 
             self.recycle_schedule = recycle_sampling["by_batch"](self.config.loader_params.maxcycle, 
@@ -728,9 +773,11 @@ class AF3TrainerRollout(AF3Trainer):
 
         #clear up memory
         del output_stack
+        print('B:', outputs['X_L'].shape[0])
 
 
         metrics_dict = self.metrics(network_input, outputs, loss_input)
+        print(metrics_dict)
         # loss, loss_dict_batched = self.loss(
         #     network_input,
         #     outputs,

rf2aa/model/af3_with_rollout.py
@@ -30,7 +30,8 @@ class AF3_with_rollout(nn.Module):
             # crd_mask_I, 
             seq, 
             rep_atom_idxs,
-            no_sync
+            no_sync,
+            frame_atom_idxs=None
         ):
         # first do forward pass
         with torch.no_grad():
@@ -50,7 +51,7 @@ class AF3_with_rollout(nn.Module):
                 D=self.config.dataset_params.diffusion_batch_size_rollout
             )
 
-            print('diffusion_output:', diffusion_output["X_L"].shape)
+            # print('diffusion_output:', diffusion_output["X_L"].shape)
             B = diffusion_output["X_L"].shape[0]
             
             # find ground truth permutation
@@ -97,9 +98,10 @@ class AF3_with_rollout(nn.Module):
                     trunk_output["S_inputs_I"],
                     trunk_output["S_I"],
                     trunk_output["Z_II"],
-                    diffusion_output["X_L"][0].unsqueeze(0),
+                    diffusion_output["X_L"][i].unsqueeze(0),
                     seq,
                     rep_atom_idxs,
+                    frame_atom_idxs=frame_atom_idxs,
                     use_reentrant=False
                     )
 
@@ -123,8 +125,8 @@ class AF3_with_rollout(nn.Module):
             distogram=None,
         )
     
-    def confidence_wrapper(self, S_inputs_I, S_I, Z_II, X_L, seq, rep_atom_idxs, use_amp=True):
-        return self.confidence(S_inputs_I, S_I, Z_II, X_L, seq, rep_atom_idxs, use_amp=use_amp)
+    def confidence_wrapper(self, S_inputs_I, S_I, Z_II, X_L, seq, rep_atom_idxs, frame_atom_idxs=None, use_amp=True):
+        return self.confidence(S_inputs_I, S_I, Z_II, X_L, seq, rep_atom_idxs, frame_atom_idxs=frame_atom_idxs, use_amp=use_amp)
 
 class ConfidenceLoss(nn.Module):
 
@@ -319,9 +321,6 @@ class ConfidenceLoss(nn.Module):
         X_prime = X_pred_I.reshape(N, L*natoms, -1, 3).repeat(1,1,ChemData().NFRAMES,1)
         Y_prime = X_gt_I.reshape(N, L*natoms, -1, 3).repeat(1,1,ChemData().NFRAMES,1)
         frames_reindex_batched, frame_mask_batched = mask_unresolved_frames_batched(frames, frame_mask, atom_mask)
-
-        if torch.sum(frame_mask_batched) == 0:
-            return torch.zeros(B, X_pred_I.shape[1], X_pred_I.shape[1], device=X_pred_I.device, dtype=torch.int64).detach(), torch.zeros(B, 64, X_pred_I.shape[1], X_pred_I.shape[1], device=X_pred_I.device, dtype=torch.bfloat16), torch.zeros(B, X_pred_I.shape[1], X_pred_I.shape[1], device=X_pred_I.device, dtype=torch.bool)
 
         X_x = torch.gather(X_prime, 1, frames_reindex_batched[...,0:1].repeat(1,1,1,3))
         X_y = torch.gather(X_prime, 1, frames_reindex_batched[...,1:2].repeat(1,1,1,3))

rf2aa/model/layers/af3_auxiliary_heads.py
@@ -20,6 +20,7 @@ class ConfidenceHead(nn.Module):
                 n_bins_pde,
                 n_bins_plddt,
                 n_bins_exp_resolved,
+                use_Cb_distances=False
                  ):
         super(ConfidenceHead, self).__init__()
         self.process_s_inputs_right = linearNoBias(449, c_z)
@@ -32,6 +33,9 @@ class ConfidenceHead(nn.Module):
         self.predict_pde = linearNoBias(c_z, n_bins_pde)
         self.predict_plddt = linearNoBias(c_s, ChemData().NHEAVY* n_bins_plddt)
         self.predict_exp_resolved = linearNoBias(c_s, ChemData().NHEAVY * n_bins_exp_resolved)
+        self.use_Cb_distances = use_Cb_distances
+        if self.use_Cb_distances:
+            self.process_Cb_distances = linearNoBias(11, c_z)
 
     def reset_parameters(self):
         for m in self.modules():
@@ -50,7 +54,8 @@ class ConfidenceHead(nn.Module):
                 #f,
                 seq,
                 rep_atoms,
-                use_amp=True
+                use_amp=True,
+                frame_atom_idxs=None
                 ):
         with torch.amp.autocast('cuda',enabled=use_amp, dtype=torch.bfloat16):
 
@@ -88,11 +93,18 @@ class ConfidenceHead(nn.Module):
             dist_one_hot = F.one_hot(discretize_distance_matrix(dist, min_distance=3.375, max_distance=20.875, num_bins=10), num_classes=11)
             Z_trunk_II = Z_trunk_II + self.process_pred_distances(dist_one_hot.float())
 
+            if self.use_Cb_distances:
+                # embed difference between observed cb and ideal cb positions
+                Cb_distances = calc_Cb_distances(X_pred_L, seq, rep_atoms, frame_atom_idxs)
+                Cb_distances_one_hot = F.one_hot(discretize_distance_matrix(Cb_distances, min_distance=0.0001, max_distance=0.25, num_bins=10), num_classes=11)
+                Z_trunk_II = Z_trunk_II + self.process_Cb_distances(Cb_distances_one_hot.float())
+
             # process with pairformer stack
             S_trunk_residual_I = S_trunk_I.clone()
             Z_trunk_residual_II = Z_trunk_II.clone()
             for n in range(len(self.pairformer)):
                 S_trunk_I, Z_trunk_II = checkpoint.checkpoint(self.pairformer[n], S_trunk_I, Z_trunk_II, use_reentrant=False)
+                # S_trunk_I, Z_trunk_II = self.pairformer[n](S_trunk_I, Z_trunk_II)
             S_trunk_I = S_trunk_residual_I + S_trunk_I
             Z_trunk_II = Z_trunk_residual_II + Z_trunk_II 
 
@@ -144,3 +156,31 @@ def find_rep_atoms(seq):
     )
     rep_atoms = torch.gather(absolute_indices, 1, idx_to_use[:,None])
     return rep_atoms[:, 0]
+
+def calc_Cb_distances(X_pred_L, seq, rep_atoms, frame_atom_idxs):
+    frame_atom_idxs = frame_atom_idxs.unsqueeze(0).expand(X_pred_L.shape[0], -1, -1)
+    # print('X_pred_L', X_pred_L.shape)
+    # print('frame_atom_idxs', frame_atom_idxs.shape)
+    N = torch.gather(X_pred_L, 1, frame_atom_idxs[...,0].unsqueeze(-1).expand(-1, -1, 3))
+    Ca = torch.gather(X_pred_L, 1, frame_atom_idxs[...,1].unsqueeze(-1).expand(-1, -1, 3))
+    C = torch.gather(X_pred_L, 1, frame_atom_idxs[...,2].unsqueeze(-1).expand(-1, -1, 3))
+    Cb = X_pred_L.index_select(1, rep_atoms)
+
+    is_valid_Cb = (seq != ChemData().aa2num["UNK"]) & (seq != ChemData().aa2num["GLY"]) & (seq != ChemData().aa2num["MAS"])
+    is_valid_Cb = is_valid_Cb & rf2aa.util.is_protein(seq)
+
+    b = Ca - N
+    c = C - Ca
+    a = torch.cross(b, c, dim=-1)
+
+    ideal_Cb = -0.58273431*a + 0.56802827*b - 0.54067466*c + Ca
+    
+    Cb_distances = torch.norm(Cb - ideal_Cb, dim=-1)
+    print('cb', Cb)
+    print('ideal_Cb', ideal_Cb)
+    Cb_distances[:,~is_valid_Cb] = 0.0
+    print('cb distances', Cb_distances)
+    print('number of valid cb atoms', is_valid_Cb.sum())
+    print('Cb_distances', Cb_distances.shape, Cb_distances[:,is_valid_Cb].mean(), Cb_distances[:,is_valid_Cb].std(), Cb_distances[:,is_valid_Cb].min(), Cb_distances[:,is_valid_Cb].max())
+
+    return Cb_distances