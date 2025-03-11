85f620151cf143bba5235ed2a4363e37e9c068e0
HaotianZhangAI4Science <haotianzhang@zju.edu.cn>
2025-03-11 16:57:39 -0700

    layer norm should only operate on the feature dim

--- rf2aa/model/layers/af3_auxiliary_heads.py
+++ rf2aa/model/layers/af3_auxiliary_heads.py
@@ -100,11 +100,11 @@ class ConfidenceHead(nn.Module):
             seq = seq.detach()
 
             # do a layer norm on S_trunk_I
-            S_trunk_I = F.layer_norm(S_trunk_I, normalized_shape=(S_trunk_I.shape))
+            S_trunk_I = F.layer_norm(S_trunk_I, normalized_shape=(S_trunk_I.shape[-1]))
             # do a layer norm on Z_trunk_II
-            Z_trunk_II = F.layer_norm(Z_trunk_II, normalized_shape=(Z_trunk_II.shape))
+            Z_trunk_II = F.layer_norm(Z_trunk_II, normalized_shape=(Z_trunk_II.shape[-1]))
             # do a layer norm on S_inputs_I
-            S_inputs_I = F.layer_norm(S_inputs_I, normalized_shape=(S_inputs_I.shape))
+            S_inputs_I = F.layer_norm(S_inputs_I, normalized_shape=(S_inputs_I.shape[-1]))
 
             # for debugging, make pair zero
             # Z_trunk_II = torch.zeros_like(Z_trunk_II, dtype=Z_trunk_II.dtype)
