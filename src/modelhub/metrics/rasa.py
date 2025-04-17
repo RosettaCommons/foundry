e661b96b4210a864a2d5a59f2de096edf281025d
Rohith Krishna <rohith@localhost>
2025-04-16 21:19:57 -0700

    add rasa.py

---
+++ src/modelhub/metrics/rasa.py
@@ -0,0 +1,64 @@
+from beartype.typing import Any
+from biotite.structure import AtomArrayStack
+import numpy as np
+
+from datahub.transforms.sasa import calculate_atomwise_rasa
+
+from modelhub.metrics.base import Metric
+
+
+class UnresolvedRegionRASA(Metric):
+    """
+    This metric computes the RASA score for unresolved regions in a protein structure.
+    The RASA score is defined as the ratio of the solvent-accessible surface area (SASA)
+    of a residue in a protein structure to the SASA of the same residue in an extended conformation.
+    """
+    @property
+    def kwargs_to_compute_args(self) -> dict[str, Any]:
+        return {
+            "predicted_atom_array_stack": ("predicted_atom_array_stack",),
+            "ground_truth_atom_array_stack": ("ground_truth_atom_array_stack",),
+        }
+    
+    def compute(
+            self,
+            predicted_atom_array_stack: AtomArrayStack,
+            ground_truth_atom_array_stack: AtomArrayStack,
+            probe_radius: float = 1.4,
+            atom_radii: str | np.ndarray = "ProtOr",
+            point_number: int = 100,
+    ) -> dict[str, Any]:
+        """
+        Compute the RASA score for unresolved regions in a protein structure.
+        
+        Args:
+            atom_array (AtomArray): The input atom array representing the protein structure.
+            probe_radius (float, optional): Van-der-Waals radius of the probe in Angstrom. Defaults to 1.4 (for water).
+            atom_radii (str | np.ndarray, optional): Atom radii set to use for calculation. Defaults to "ProtOr".
+            point_number (int, optional): Number of points in the Shrake-Rupley algorithm to sample for calculating SASA. Defaults to 100.
+            
+        Returns:
+            dict: A dictionary containing the RASA score and other relevant information.
+        """
+
+        # find unresolved regions
+        # (polymer atoms with occupancy 0.0)
+        atoms_to_score = ground_truth_atom_array_stack.is_polymer & (ground_truth_atom_array_stack.occupancy == 0.0)
+        rasas = []
+        # Calculate RASA
+        for atom_array in predicted_atom_array_stack:
+            rasa = calculate_atomwise_rasa(
+                atom_array=atom_array,
+                probe_radius=probe_radius,
+                atom_radii=atom_radii,
+                point_number=point_number,
+            )
+            rasas.append(rasa[atoms_to_score].mean())
+        # Calculate the mean RASA score
+
+        rasa = np.nanmean(rasas) 
+        output_dictionary = {
+            f"unresolved_polymer_rasa_batch{i}": rasa for i, rasa in enumerate(rasas)
+        }
+        output_dictionary["mean_unresolved_polymer_rasa"] = rasa
+        return output_dictionary
