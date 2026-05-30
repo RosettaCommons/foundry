4bdc24c7aa57d41db41300d1dfa4b86214f0277b
Rachel Clune <rachel.clune@omsf.io>
2026-05-30 10:40:50 -0700

    First draft of binder design tutorial
    The original tutorial was written by Julia Bonzanini with support from Christian Schellhaas during the 2026 Tutorial Hackathon.
    
    PDF1 example still needs to be edited.

---
+++ models/rfd3/docs/tutorials/binder_design_tutorial_files/CD28/cd28.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --partition=h100
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:1
+#SBATCH --mem 32gb
+#SBATCH --time 00:59:00
+#SBATCH --job-name="rfd3"
+
+source ~/.bashrc
+conda activate rc-foundry
+
+# Set variables
+INFILE="./cd28.yaml" #or .json
+OUTDIR="./diffusion_outs"
+
+# Run RFdiffusion3
+rfd3 design \
+  out_dir="$OUTDIR" \
+  inputs="$INFILE" \
+  n_batches=1 \
+  diffusion_batch_size=1 \
+  dump_trajectories=1 # OPTIONAL, FOR VISUALIZATION PURPOSES
+
