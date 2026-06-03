9fbeffc5908768f887c454b8cacb3f8f69104ff8
Rachel Clune <rachel.clune@omsf.io>
2026-06-03 10:00:59 -0700

    Minor binder design tutorial edits
    - Fixed cd28_nag.pdb: the F chain NAG molecule did not have a residue number
    - Updated the example runscript files to remove any system/user-specific information. I also removed the variables as they seemed unnecessary for such a simple runscript.
    - Added example output files for CD28 and PDF1
    - Added a 'Step 3' section for the small molecule example
    - Corrected noisy and denoised trajectory file descriptions in the tutorial text.

--- models/rfd3/docs/tutorials/binder_design_tutorial_files/CD28/cd28.sh
+++ models/rfd3/docs/tutorials/binder_design_tutorial_files/CD28/cd28.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --partition=h100
+#SBATCH --partition=<your_partition>
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
@@ -8,18 +8,11 @@
 #SBATCH --time 00:59:00
 #SBATCH --job-name="rfd3"
 
-source ~/.bashrc
-conda activate rc-foundry
-
-# Set variables
-INFILE="./cd28.yaml" #or .json
-OUTDIR="./diffusion_outs"
-
 # Run RFdiffusion3
 rfd3 design \
-  out_dir="$OUTDIR" \
-  inputs="$INFILE" \
+  out_dir="./cd28_nag_binder_outputs" \
+  inputs="./cd28.yaml" \
   n_batches=1 \
-  diffusion_batch_size=1 \
+  diffusion_batch_size=8 \
   dump_trajectories=1 # OPTIONAL, FOR VISUALIZATION PURPOSES
 
